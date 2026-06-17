73d469faf708718e60284f85c4112ff29436f4f8
Vivekanand Pujar <vivek@vivekanand.dev>
2026-06-17 15:32:55 +0530

    formatting and redundancy removal

--- modules/network-ec2/main.tf
+++ modules/network-ec2/main.tf
@@ -12,7 +12,6 @@ locals {
 }
 
 resource "aws_vpc" "main" {
-  region               = var.aws_region
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
